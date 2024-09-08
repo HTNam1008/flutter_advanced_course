10b76ad4175facea6756d09fffc26af506ea1078
HThanh Nam <dev.hoangthanhnam.1008@gmail.com>
2024-09-08 22:07:53 +0700

    Lecture 18 Adding Time to Splash Screen

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/presentation/resources/assets_manager.dart';
 import 'package:flutter_application_1/presentation/resources/color_manager.dart';
+import 'package:flutter_application_1/presentation/resources/route_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,6 +13,28 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), _goNext);
+  }
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _timer?.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
