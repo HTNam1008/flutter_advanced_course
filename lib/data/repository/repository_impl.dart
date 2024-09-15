94957c7bf564caa88c3b7edc5e2725d3eaee11ab
HThanh Nam <dev.hoangthanhnam.1008@gmail.com>
2024-09-15 14:00:32 +0700

    Lecture 31: Applying ErrorHandler on Repository Implementer

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_application_1/data/data_source/remote_data_source.dart';
 import 'package:flutter_application_1/data/mapper/mapper.dart';
+import 'package:flutter_application_1/data/network/error_handler.dart';
 import 'package:flutter_application_1/data/network/failure.dart';
 import 'package:flutter_application_1/data/network/network_info.dart';
 import 'package:flutter_application_1/data/request/request.dart';
@@ -17,21 +18,26 @@ class RepositoryImpl implements  Repository {
   Future<Either<Failure, Authentication>> login(
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
-      // its safe to call
-      final response = await _remoteDataSource.login(loginRequest);
-      if (response.status == 0) {
-        // return data (success)
-        // return right
-        return Right(response.toDomain());
-      } else {
-        // return business logic error
-        // return left
-        return Left(Failure(
-            409, response.message ?? "we have business error from API side"));
+      try {
+        // its safe to call
+        final response = await _remoteDataSource.login(loginRequest);
+
+        if (response.status == ApiInternalStatus.SUCCESS) {
+          // return data (success)
+          // return right
+          return Right(response.toDomain());
+        } else {
+          // return business logic error
+          // return left
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
       }
     } else {
       // return connection error
-      return Left(Failure(501, "please check your internet connection"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
