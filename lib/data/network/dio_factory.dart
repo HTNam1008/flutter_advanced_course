a3e5b3e915ff25cfde3a83abd2053beaf3bab7b6
HThanh Nam <dev.hoangthanhnam.1008@gmail.com>
2024-09-15 15:06:02 +0700

    Lecture 32: Adding Dio Factory

---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/app/constant.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constant.token,
+      DEFAULT_LANGUAGE: "en", // TODO: get lang from app prefs
+    };
+
+    const timeOut = 60; // 1 min
+
+    dio.options = BaseOptions(
+      baseUrl: Constant.baseUrl,
+      connectTimeout: const Duration(seconds: timeOut),
+      receiveTimeout: const Duration(seconds: timeOut),
+      headers: headers,
+    );
+
+    return dio;
+  }
+}
